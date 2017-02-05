cd758aea6262b0e5848e6d9c6f46a6a130bed025
katelyj <kjohnston@stuy.edu>
2017-02-05 16:00:25 -0500

    this doesnt work

--- db_builder.py
+++ db_builder.py
@@ -1,7 +1,35 @@
 from pymongo import MongoClient
+import csv
+
+
+#setting up database
 
 server = MongoClient('149.89.150.100')
 db = server.PokeMongo
 c = db.students
 
-c.count()
+
+#csv data
+
+f1 = open("peeps.csv", "r")
+peeps = csv.DictReader(f1)
+
+f2 = open("courses.csv", "r")
+courses = csv.DictReader(f2)
+
+
+#putting data in the database
+
+for p in peeps:
+	d = {}
+	d['name'] = p['name']
+	d['age'] = int(p['age'])
+	d['id'] = int(p['id'])
+	for co in courses:
+		if co['id'] == p['id']:
+			d[co['code']] = int(co['mark'])
+	f2.seek(0)
+	c.insert_one(d)
+
+f1.close()
+f2.close()
