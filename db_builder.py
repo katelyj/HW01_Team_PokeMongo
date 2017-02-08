3d0df1a43252237b496f37162f97133fc8485a99
Zzcnick <zzhen@stuy.edu>
2017-02-08 00:55:24 -0500

    add averages

--- db_builder.py
+++ db_builder.py
@@ -4,8 +4,8 @@ import csv
 
 #setting up database
 
-#server = MongoClient('127.0.0.1')
-server = MongoClient('149.89.150.100')
+server = MongoClient('127.0.0.1')
+#server = MongoClient('149.89.150.100')
 db = server.PokeMongo
 c = db.students
 c.remove() # Clear Collection
@@ -20,17 +20,18 @@ peeps = csv.DictReader(f1)
 f2 = open("courses.csv", "r")
 courses = csv.DictReader(f2)
 
-
 #putting data in the database
 for p in peeps:
-
     d = {}
     d['name'] = p['name']
     d['age'] = int(p['age'])
     d['id'] = int(p['id'])
+    course_list = []
     for co in courses:
         if co['id'] == p['id']:
-            d[co['code']] = int(co['mark'])
+            course_list += [ {'code' : co['code'], 
+                              'mark' : int(co['mark']) } ]
+    d['courses'] = course_list
     print d
     c.insert_one(d)
     f2.seek(0)
--- db_builder1.py
+++ db_builder1.py
@@ -1,10 +1,29 @@
 from pymongo import MongoClient
 import csv
 
+def average(data):
+    classes = len(data)
+    if classes == 0:
+        return "N/A"
+    total = 0
+    for course in data:
+        total += course['mark']
+    return float(total) / classes
+
 
 #setting up database
 
-server = MongoClient('149.89.150.100')
+server = MongoClient('127.0.0.1')
+#server = MongoClient('149.89.150.100')
 db = server.PokeMongo
 c = db.students
-c.remove()
+
+# printing averages
+
+students = c.find()
+for student in students:
+    print "Name: %-*sID: %s\tAverage: %.2f"%(15, student['name'],
+                                              student['id'],
+                                              average(student['courses']))
+
+# populating with teachers
